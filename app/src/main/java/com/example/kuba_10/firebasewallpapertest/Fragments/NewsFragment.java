a5ed9faaab8d064de794362e4c3b8fa8c130b39b
KubaStachecki <jakub.stachecki@gmail.com>
2017-07-19 13:27:46 +0200

    retro gaming mockup 1

--- app/src/main/java/com/example/kuba_10/firebasewallpapertest/Fragments/NewsFragment.java
+++ app/src/main/java/com/example/kuba_10/firebasewallpapertest/Fragments/NewsFragment.java
@@ -1,8 +1,42 @@
 package com.example.kuba_10.firebasewallpapertest.Fragments;
 
-/**
- * Created by Kuba-10 on 19.07.2017.
- */
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class NewsFragment {
+import com.example.kuba_10.firebasewallpapertest.R;
+
+public class NewsFragment extends Fragment {
+
+
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static NewsFragment newInstance() {
+        NewsFragment fragment = new NewsFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+
+
 }
--- app/src/main/java/com/example/kuba_10/firebasewallpapertest/MainActivity.java
+++ app/src/main/java/com/example/kuba_10/firebasewallpapertest/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.kuba_10.firebasewallpapertest;
 
-import android.app.ActionBar;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
 
 import com.example.kuba_10.firebasewallpapertest.Adapters.WallAdapter;
 import com.example.kuba_10.firebasewallpapertest.Fragments.FragmentUtils;
 import com.example.kuba_10.firebasewallpapertest.Fragments.GalleryFragment;
+import com.example.kuba_10.firebasewallpapertest.Fragments.MapFragment;
+import com.example.kuba_10.firebasewallpapertest.Fragments.NewsFragment;
 import com.example.kuba_10.firebasewallpapertest.Fragments.SplashFragment;
+import com.example.kuba_10.firebasewallpapertest.Fragments.StartFragment;
 import com.example.kuba_10.firebasewallpapertest.Model.Image;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,12 +25,134 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements FragmentUtils {
+public class MainActivity extends AppCompatActivity implements FragmentUtils, View.OnClickListener {
 
 
     public static final String TAAAAG = "TAAAAG";
-
     private static final int REQUEST_WRITE_PERMISSION = 786;
+
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3, floatingActionButton4;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton1 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
+        floatingActionButton2 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item2);
+        floatingActionButton3 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item3);
+        floatingActionButton4 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item4);
+
+        floatingActionButton1.setOnClickListener(this);
+        floatingActionButton2.setOnClickListener(this);
+        floatingActionButton3.setOnClickListener(this);
+        floatingActionButton3.setOnClickListener(this);
+        floatingActionButton4.setOnClickListener(this);
+
+
+        hideTaskAndActrionBar();
+        requestPermission();
+
+        SplashFragment.newInstance().show(getSupportFragmentManager(), "");
+
+
+        openFragment(StartFragment.newInstance());
+
+
+
+
+
+    }
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        hideTaskAndActrionBar();
+    }
+
+
+
+
+
+    @Override
+    public void openFragment(Fragment fragment) {
+
+        this.getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit)
+                .replace(R.id.main_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+
+
+
+
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+
+
+            case R.id.material_design_floating_action_menu_item1:
+
+                openFragment(MapFragment.newInstance());
+
+                break;
+
+
+            case R.id.material_design_floating_action_menu_item2:
+
+
+                openFragment(StartFragment.newInstance());
+
+                break;
+
+            case R.id.material_design_floating_action_menu_item3:
+
+
+                openFragment(NewsFragment.newInstance());
+
+                break;
+
+
+            case R.id.material_design_floating_action_menu_item4:
+
+
+                openFragment(GalleryFragment.newInstance());
+
+                break;
+
+
+        }
+
+
+    }
+
+
 
 
     @Override
@@ -47,78 +170,13 @@ public class MainActivity extends AppCompatActivity implements FragmentUtils {
     }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+    private void hideTaskAndActrionBar() {
         View decorView = getWindow().getDecorView();
-// Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         getSupportActionBar().hide();
-
-
-        requestPermission();
-
-        SplashFragment.newInstance().show(getSupportFragmentManager(), "");
-
-
-
-        openFragment(GalleryFragment.newInstance());
-
-
-
     }
 
-
-
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-
-        View decorView = getWindow().getDecorView();
-// Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-        getSupportActionBar().hide();
-
-
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-
-        getSupportActionBar().hide();
-    }
-
-    @Override
-    public void openFragment(Fragment fragment) {
-
-        this.getSupportFragmentManager()
-                .beginTransaction()
-//                .setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit)
-                .replace(R.id.main_container, fragment)
-                .addToBackStack(null)
-                .commit();
-    }
-
-
-//        File file = new File(this.getFilesDir(), "/kuuuuba");
-//
-//        if (!file.exists()) {
-//            if (file.mkdir()) ; //directory is created;
-//
-//            Toast.makeText(this, "zrobilem folder!", Toast.LENGTH_SHORT).show();
-//
-//        }
-//
-//        Toast.makeText(this, "folder juz byl", Toast.LENGTH_SHORT).show();
 
 
 }
